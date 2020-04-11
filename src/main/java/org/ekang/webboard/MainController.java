313388837bfa6bcdc7f82372cd52d1acbf3a4ce9
Emma Kang <emma140626@gmail.com>
2020-04-10 20:55:23 -0400

    initial commit

---
+++ src/main/java/org/ekang/webboard/MainController.java
@@ -0,0 +1,14 @@
+package org.ekang.webboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String Main() {
+        return "index";
+    }
+}
---
+++ src/main/web/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main</title>
+</head>
+
+<body>
+Hello
+</body>
+</html>
